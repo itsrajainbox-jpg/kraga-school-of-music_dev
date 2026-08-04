b9ae2b275d6e5ed3cf06fb350e5bb20c6fe482a5
itsrajainbox-jpg <itsraja.inbox@gmail.com>
2026-08-04 08:34:10 +0530

    fix: re-enable GlobalStudentsSection on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ export default function HomePage() {
       </section>
       <SocialProofSection />
       <PianoJourneySection />
-      {/* <GlobalStudentsSection /> */}
+      <GlobalStudentsSection />
       <WhyStudentsChooseKraga />
       {/* <CTASection /> */}
     </>
